f4877f3dc4cfc82f89a496940fd23781de04f2c4
Miguel Somohano <miguel.spinis@gmail.com>
2020-07-06 01:00:04 -0500

    Agregando validacion al formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Error from './Error'
 
 const Formulario = () => {
+
+    // state
+    const [ termino, guardarTermino ] = useState('');
+    const [ error, guardarError ] = useState(false);
+
+    const buscarImagenes = e => {
+        e.preventDefault();
+
+        // Validacion
+        if(termino.trim()==='') {
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+        // enviar  el termino  de busqueda hacia el componente principal
+
+    }
+    
+
+
     return ( 
-        <form>
+        <form
+            onSubmit={buscarImagenes}
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input 
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Busca una imagen, Ej. Futbol o Café"
+                        onChange={ e => guardarTermino( e.target.value ) }  
                     />
                 </div>
                 <div className="form-group col-md-4">
                     <input 
-                        type="sumbit"
+                        type="submit"
                         className="btn btn-lg btn-danger btn-block"
                         value="Buscar"
                     />
                 </div>
             </div>
+            { error ? <Error mensaje="Agrega un término de búsqueda"/> : null}
         </form>
      );
 }
